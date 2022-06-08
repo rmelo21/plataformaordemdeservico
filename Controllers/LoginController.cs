1753d7f421557dc421ed04901a7370a50068ba35
rmelo21 <ricardo_pdf@hotmail.com>
2022-06-08 14:15:36 -0300

    inclusao view login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using plataformaordemdeservico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plataformaordemdeservico.Controllers
+{
+    public class LoginController : Controller
+    {
+        public static List<Login> lslogin = new List<Login>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plataformaordemdeservico.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string usuario { get; set; }
+        public string senha { get; set; }
+    }
+}
